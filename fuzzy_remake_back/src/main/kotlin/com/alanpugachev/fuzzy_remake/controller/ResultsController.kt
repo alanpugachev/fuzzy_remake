960fde00c21bcb23d69e4fbe2f6ce9bdf9dcaedb
Alan Pugachev <aszneesizzle@gmail.com>
2025-07-16 23:43:39 +0300

    fix 7_16_2025_11_43_pm

--- fuzzy_remake_back/src/main/kotlin/com/alanpugachev/fuzzy_remake/controller/ResultsController.kt
+++ fuzzy_remake_back/src/main/kotlin/com/alanpugachev/fuzzy_remake/controller/ResultsController.kt
@@ -2,12 +2,14 @@ package com.alanpugachev.fuzzy_remake.controller
 
 import com.alanpugachev.fuzzy_remake.consumer.SurveyProcessedResultConsumer
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api/results")
+@RestController
+@RequestMapping("/api/results")
+@CrossOrigin(origins = ["http://localhost:3000"]) /* only for testing */
 class ResultsController(
     private val surveyProcessedResultConsumer: SurveyProcessedResultConsumer
 ) {
