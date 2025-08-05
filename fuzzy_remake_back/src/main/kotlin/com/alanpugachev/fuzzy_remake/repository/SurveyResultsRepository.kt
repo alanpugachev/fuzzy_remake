3d33703751f42d3559c84f64653e851bc18f9a05
Alan Pugachev <aszneesizzle@gmail.com>
2025-08-06 01:06:08 +0300

    add test get controller for survey results

--- fuzzy_remake_back/src/main/kotlin/com/alanpugachev/fuzzy_remake/repository/SurveyResultsRepository.kt
+++ fuzzy_remake_back/src/main/kotlin/com/alanpugachev/fuzzy_remake/repository/SurveyResultsRepository.kt
@@ -2,9 +2,22 @@ package com.alanpugachev.fuzzy_remake.repository
 
 import com.alanpugachev.fuzzy_remake.entity.SurveyResults
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface SurveyResultsRepository : JpaRepository<SurveyResults, Long> {
     fun save(surveyResults: SurveyResults): SurveyResults
+
+    fun getByUserId(userId: Long): SurveyResults
+
+    @Query(
+        value = """
+            SELECT sr FROM SurveyResults as sr
+            WHERE sr.userId = :userId
+            ORDER BY sr.createdAt DESC
+            LIMIT 1
+        """
+    )
+    fun getLastByUserId(userId: Long): SurveyResults
 }
